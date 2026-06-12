a386e0a6a2650b4b2f9fd5804af495aabb19b2d5
Precipitator80 <rainlandsociety@gmail.com>
2026-06-13 00:09:45 +0100

    Changed incorrect settings value to not show for level generation errors.

--- src/HintImageManager.cs
+++ src/HintImageManager.cs
@@ -50,7 +50,7 @@ namespace MonstrumExtendedSettingsMod
                         { "EscapeConditions3", ModSettings.escapeConditionsToWin == 3 },
                         { "GlowstickHunt", ModSettings.glowstickHunt && !ModSettings.noGlowstickHuntFinale },
                         { "GlowstickHuntNoFinale", ModSettings.glowstickHunt && ModSettings.noGlowstickHuntFinale },
-                        { "IncorrectSettingValues", ModSettings.errorWhileReadingModSettings || ModSettings.errorDuringLevelGeneration },
+                        { "IncorrectSettingValues", ModSettings.errorWhileReadingModSettings },
                         { "RestartRequired", ModSettings.restartSettingsChanged.Count > 0 }
                     };
 
